f909c65815a063405a791c338a1425133148f4b1
mannylopez <manny@mapbox.com>
2015-11-16 15:45:25 -0800

    add files

---
+++ assets/js/garmin.js
@@ -0,0 +1,66 @@
+
+
+var one = omnivore.gpx('assets/data/1.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/2.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/3.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/4.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/5.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/6.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/7.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/8.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/9.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/10.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/11.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/12.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/13.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/14.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/15.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/16.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/17.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/18.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/19.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/20.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/21.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/22.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/23.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/24.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/25.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/26.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/27.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/28.gpx').addTo(map);
+
+// var locationTwo = omnivore.gpx('assets/data/activity_892802983.gpx').addTo(map);
+
+// var locationThree = omnivore.gpx('assets/data/activity_892802985.gpx').addTo(map);
+
+// var locationFour = omnivore.gpx('assets/data/activity_892802990.gpx').addTo(map);
+
+// var locationFive = omnivore.gpx('assets/data/activity_892802998.gpx').addTo(map);
+
+// var locationSix = omnivore.gpx('assets/data/activity_892803004.gpx').addTo(map);
+
+// var locationSeven = omnivore.gpx('assets/data/activity_892803016.gpx').addTo(map);
+
+// var locationEight = omnivore.gpx('assets/data/activity_892803035.gpx').addTo(map);
+
+// var locationNine = omnivore.gpx('assets/data/activity_892803042.gpx').addTo(map);
+
+// var locationTen = omnivore.gpx('assets/data/activity_892803049.gpx').addTo(map);
+
+// var locationEleven = omnivore.gpx('assets/data/activity_892803065.gpx').addTo(map);
+
+// var locationTwelve = omnivore.gpx('assets/data/activity_892803073.gpx').addTo(map);
+
+// var locationThirteen = omnivore.gpx('assets/data/activity_892803081.gpx').addTo(map);
+
+// var locationFourteen = omnivore.gpx('assets/data/activity_893736148.gpx').addTo(map);
+
+// var locationFifteen = omnivore.gpx('assets/data/activity_893736175.gpx').addTo(map);
+
+// var locationSixteen = omnivore.gpx('assets/data/activity_893736197.gpx').addTo(map);
+
+// var locationSeventeen = omnivore.gpx('assets/data/activity_893736208.gpx').addTo(map);
+
+// var locationEighteen = omnivore.gpx('assets/data/activity_893736220.gpx').addTo(map);
+
+// var locationNineteen = omnivore.gpx('assets/data/activity_893736236.gpx').addTo(map);
