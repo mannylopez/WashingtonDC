c9c82d2827a7017cf4b65836a1043fc187c6b60c
mannylopez <manny@mapbox.com>
2015-12-02 22:48:17 -0500

    first commit

--- assets/js/garmin.js
+++ assets/js/garmin.js
@@ -1,4 +1,7 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoibWFubnkiLCJhIjoiMzBCNHFQUSJ9.VQQ9ZSW7viFT1yLhLiWLSA';
 
+var map = L.mapbox.map('map', 'mapbox.streets')
+  .setView([38.918209, -77.036801], 12);
 
 var one = omnivore.gpx('assets/data/1.gpx').addTo(map);
 var one = omnivore.gpx('assets/data/2.gpx').addTo(map);
@@ -28,39 +31,3 @@ var one = omnivore.gpx('assets/data/25.gpx').addTo(map);
 var one = omnivore.gpx('assets/data/26.gpx').addTo(map);
 var one = omnivore.gpx('assets/data/27.gpx').addTo(map);
 var one = omnivore.gpx('assets/data/28.gpx').addTo(map);
-
-// var locationTwo = omnivore.gpx('assets/data/activity_892802983.gpx').addTo(map);
-
-// var locationThree = omnivore.gpx('assets/data/activity_892802985.gpx').addTo(map);
-
-// var locationFour = omnivore.gpx('assets/data/activity_892802990.gpx').addTo(map);
-
-// var locationFive = omnivore.gpx('assets/data/activity_892802998.gpx').addTo(map);
-
-// var locationSix = omnivore.gpx('assets/data/activity_892803004.gpx').addTo(map);
-
-// var locationSeven = omnivore.gpx('assets/data/activity_892803016.gpx').addTo(map);
-
-// var locationEight = omnivore.gpx('assets/data/activity_892803035.gpx').addTo(map);
-
-// var locationNine = omnivore.gpx('assets/data/activity_892803042.gpx').addTo(map);
-
-// var locationTen = omnivore.gpx('assets/data/activity_892803049.gpx').addTo(map);
-
-// var locationEleven = omnivore.gpx('assets/data/activity_892803065.gpx').addTo(map);
-
-// var locationTwelve = omnivore.gpx('assets/data/activity_892803073.gpx').addTo(map);
-
-// var locationThirteen = omnivore.gpx('assets/data/activity_892803081.gpx').addTo(map);
-
-// var locationFourteen = omnivore.gpx('assets/data/activity_893736148.gpx').addTo(map);
-
-// var locationFifteen = omnivore.gpx('assets/data/activity_893736175.gpx').addTo(map);
-
-// var locationSixteen = omnivore.gpx('assets/data/activity_893736197.gpx').addTo(map);
-
-// var locationSeventeen = omnivore.gpx('assets/data/activity_893736208.gpx').addTo(map);
-
-// var locationEighteen = omnivore.gpx('assets/data/activity_893736220.gpx').addTo(map);
-
-// var locationNineteen = omnivore.gpx('assets/data/activity_893736236.gpx').addTo(map);
